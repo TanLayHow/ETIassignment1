d063ab325205831a3775755a7937171931fa699e
TanLayHow <s10206171@connect.np.edu.sg>
2022-12-21 19:24:11 +0800

    Update ETI_Script.sql
    Final sql script used

--- ETI_Script.sql
+++ ETI_Script.sql
@@ -1,44 +1,58 @@
-DROP DATABASE ETI_Passengers;
+/* Passengers */
+DROP DATABASE IF EXISTS ETI_Passengers;
 CREATE DATABASE ETI_Passengers;
 USE ETI_Passengers;
-
-DROP TABLE Passengers;
+DROP TABLE IF EXISTS Passengers;
 CREATE TABLE Passengers (
-    PassengerID varchar(255) NOT NULL,
-    FirstName varchar(255) NOT NULL,
-    LastName varchar(255),
-    MobileNumber varchar(255) UNIQUE,
-    Email varchar(255) UNIQUE,
+    PassengerID VARCHAR(255) NOT NULL,
+    FirstName VARCHAR(255) NOT NULL,
+    LastName VARCHAR(255),
+    MobileNumber VARCHAR(255) UNIQUE,
+    Email VARCHAR(255) UNIQUE,
     PRIMARY KEY (PassengerID)
 );
-
-DROP TABLE PassengerRides;
+INSERT INTO Passengers (PassengerID, FirstName, LastName, MobileNumber, Email) VALUES("PID12345678", "Tom", "William", "88096390", "TomWilliam@gmail.com");
+INSERT INTO Passengers (PassengerID, FirstName, LastName, MobileNumber, Email) VALUES("PID55566688", "Daniel", "Dan", "88223344", "DanielDan@gmail.com");
+INSERT INTO Passengers (PassengerID, FirstName, LastName, MobileNumber, Email) VALUES("PID98712388", "John", "Doe", "91192628", "JohnDoe@gmail.com");
+SELECT * FROM Passengers;
+/* Passenger Rides */
+DROP TABLE IF EXISTS PassengerRides;
 CREATE TABLE PassengerRides (
-    RideID varchar(255) NOT NULL,
-    RideDate DateTime,
-    PassengerID varchar(255) NOT NULL,
-    PickupCode varchar(255),
-    DropoffCode varchar(255),
-    DriverID varchar(255) NOT NULL,
-    CarLicense varchar(255),
-    RideStatus varchar(255),
-    primary key (RideID),
- foreign key (PassengerID) references Passengers (PassengerID)
-); 
+    RideID VARCHAR(255) NOT NULL,
+    RideDate DATETIME,
+    PassengerID VARCHAR(255) NOT NULL,
+	DriverID VARCHAR(255) NOT NULL,
+    PickUpPostal VARCHAR(255),
+    DropOffPostal VARCHAR(255),
+    CarLicense VARCHAR(255),
+    RideStatus VARCHAR(255),
+    PRIMARY KEY (RideID),
+	FOREIGN KEY (PassengerID) REFERENCES Passengers (PassengerID)
+);	
 
-DROP DATABASE ETI_Drivers;
+/* USE ETI_Passengers;
+SELECT * FROM Passengers
+SELECT * FROM PassengerRides; */
+
+/* Drivers */
+DROP DATABASE IF EXISTS ETI_Drivers;
 CREATE DATABASE ETI_Drivers;
 USE ETI_Drivers;
-
-DROP TABLE Drivers;
+DROP TABLE IF EXISTS Drivers;
 CREATE TABLE Drivers (
-    DriverID varchar(255) NOT NULL,
-    FirstName varchar(255) NOT NULL,
-    LastName varchar(255),
-    MobileNumber varchar(255) UNIQUE,
-    Email varchar(255),
-    IdentificationNumber varchar(255) NOT NULL UNIQUE,
-    CarLicense varchar(255),
-    DriverStatus varchar(255),
+    DriverID VARCHAR(255) NOT NULL,
+    FirstName VARCHAR(255) NOT NULL,
+    LastName VARCHAR(255),
+    MobileNumber VARCHAR(255) UNIQUE,
+    Email VARCHAR(255) UNIQUE,
+    IdentificationNumber VARCHAR(255) NOT NULL UNIQUE,
+    CarLicense VARCHAR(255),
+    DriverStatus VARCHAR(255),
     PRIMARY KEY (DriverID)
 );
+INSERT INTO Drivers (DriverID, FirstName, LastName, MobileNumber, Email, IdentificationNumber, CarLicense, DriverStatus) VALUES("DID44448888", "Rheanna", "Yong", "93825712", "RheannaYong@gmail.com", "T0318985B", "L34567", 'Available');
+INSERT INTO Drivers (DriverID, FirstName, LastName, MobileNumber, Email, IdentificationNumber, CarLicense, DriverStatus) VALUES("DID33337777", "Mongus", "Luke", "32148591", "MongusLuke@gmail.com", "T0318484A", "L98765", 'Available');
+INSERT INTO Drivers (DriverID, FirstName, LastName, MobileNumber, Email, IdentificationNumber, CarLicense, DriverStatus) VALUES("DID12345679", "Pom", "Pom", "98375822", "PomPom@gmail.com", "S349143T", "L12345", 'Available');
+
+/* USE ETI_Driver;
+SELECT * FROM Drivers; */
